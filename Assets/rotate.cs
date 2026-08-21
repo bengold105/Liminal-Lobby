7e06ed8ad50b81f6c720b669c173513f14d48e73
Ben <bengold105@gmail.com>
2026-08-21 16:46:52 +0200

    first commit

---
+++ Assets/rotate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float rotationspeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, rotationspeed, 0f, Space.Self);
+    }
+}
